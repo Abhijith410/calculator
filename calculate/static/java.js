30d6837c10b5e6ca42c4baac3a8aa0e857257f7f
Abhijith <ayabhijith@gmail.com>
2023-01-03 11:29:45 +0530

    calc

--- calculate/static/java.js
+++ calculate/static/java.js
@@ -7,9 +7,39 @@ function display(value1) {
     return value1;
     }
       
-function calculate() {
-    var p = document.getElementById("result").value;
-    var q = eval(p);
-    document.getElementById("result").value = q;
+// function calculate() {
+//     var p = document.getElementById("result").value;
+//     var q = eval(p);
+//     document.getElementById("result").value = q;
+//     }
+
+function operators(){
+    var a = [];
+    var answer;
+    
+    if(document.getElementById("result").value.includes("+")){
+    a = document.getElementById("result").value.split('+');
+    return answer= parseFloat(a[0]) + parseFloat(a[1]);
     }
+    
+    else if(document.getElementById("result").value.includes("-")){
+    a = document.getElementById("result").value.split('-');
+    return answer= parseFloat(a[0]) - parseFloat(a[1]);
+    }
+    
+    else if(document.getElementById("result").value.includes("/")){
+    a = document.getElementById("result").value.split('/');
+    return answer= parseFloat(a[0]) / parseFloat(a[1]);
+    }
+    
+    else{
+    a = document.getElementById("result").value.split('*');
+    return answer= parseFloat(a[0]) * parseFloat(a[1]);
+    }
+}
+    
+function calculate(){	
+var b = operators()
+document.getElementById('result').value = b; 
+}
        
